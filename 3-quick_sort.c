a30551eddb3dc72a801a5724f8ade34bbf7c96f6
GetacherD <getacher.d@gmail.com>
2022-07-16 11:40:57 -0400

    test

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -14,6 +14,7 @@ void qs(int *array, size_t size, int left, int right)
 	int j =  left;
 
 	if (left >= right)
+
 		return;
 	while (j < right)
 	{
@@ -42,6 +43,10 @@ void qs(int *array, size_t size, int left, int right)
 void quick_sort(int *array, size_t size)
 {
 	if (!array || size == 1)
+	{
+		print_array(array, size);
 		return;
+	}
 	qs(array, size,  0, size - 1);
+	print_array(array, size);
 }
