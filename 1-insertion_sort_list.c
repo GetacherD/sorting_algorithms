c180d4b5571e4da261a3959a6b8789584d67d559
GetacherD <getacher.d@gmail.com>
2022-07-16 02:43:33 -0700

    test

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,89 +1,78 @@
 #include "sort.h"
 /**
- * add_to_start - helper function add element to start
- * @list: head of list
- * @cur: cur node
- * @pivot: pivot node
- */
-void add_to_start(listint_t **list, listint_t *cur, listint_t *pivot)
-{
-
-			cur->prev->next = pivot;
-			if (pivot)
-				pivot->prev = cur->prev;
-			cur->next = *list;
-			(*list)->prev = cur;
-			cur->prev = NULL;
-			*list = cur;
-			print_list(*list);
-}
-/**
- * _exec_sort - execute sort on filtered list
- * @list: head pointer
- */
-void _exec_sort(listint_t **list)
-{
-	listint_t *cur, *pivot, *mover;
-	int add_start = 1;
-
-	cur = (*list)->next;
-	pivot = cur->next;
-	mover = cur->prev;
-	while (cur)
-	{
-		while (mover)
-		{
-			if (mover->n <= cur->n)
-			{
-				cur->prev->next = pivot;
-				if (pivot)
-					pivot->prev = cur->prev;
-				mover->next->prev = cur;
-				cur->next = mover->next;
-				mover->next = cur;
-				cur->prev = mover;
-				add_start = 0;
-				print_list(*list);
-				break;
-			}
-			mover = mover->prev;
-		}
-		if (add_start == 1)
-		{
-			add_to_start(list, cur, pivot);
-			add_start = 1;
-		}
-		cur = pivot;
-		add_start = 1;
-		if (cur)
-			mover = cur->prev;
-		if (pivot)
-			pivot = pivot->next;
-	}
-}
-/**
  * insertion_sort_list - sort using insertion algorithm
  * @list: head pointer to doubly linked list
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *cur;
+	listint_t *cur, *M, *pivot;
 
 	if (!list || !(*list) || !((*list)->next))
 		return;
-	if (!(((*list)->next)->next))
+	if ((*list)->next->next == NULL)
 	{
-		if ((*list)->n > ((*list)->next)->n)
+		printf("Two Elements\n");
+		cur = *list;
+		M = cur->next;
+		if (cur->n > M->n)
 		{
-			cur = (*list)->next;
-			(*list)->prev = cur;
-			cur->prev = NULL;
-			cur->next = *list;
-			(*list)->next = NULL;
-			(*list) = cur;
+			M->next = cur;
+			cur->prev = M;
+			cur->next = NULL;
+			M->prev = NULL;
+			*list = M;
 		}
 		return;
 	}
-	_exec_sort(list);
+	M = (*list)->next;
+	cur = *list;
+	if (cur->n > M->n)
+	{
+		M = *list;
+		*list = (*list)->next;
+		cur = *list;
+		cur->prev = NULL;
+		pivot = cur->next;
+	}
+	else
+	{
+		pivot = M->next;
+		if (cur)
+			cur->next = pivot;
+		if (pivot)
+			pivot->prev = cur;
+	}
+	while (M)
+	{
+		while (cur)
+		{
+			if (cur->n < M->n)
+			{
+				M->next = cur->next;
+				M->prev = cur;
+				cur->next = M;
+				if (M->next)
+					M->next->prev = M;
+				break;
+			}
+			cur = cur->prev;
+		}
+		if (cur == NULL)
+		{
+			M->next = *list;
+			M->prev = NULL;
+			*list = M;
+		}
+		M = pivot;
+		if (M)
+		{
+			cur = M->prev;
+			pivot = M->next;
+			if (cur)
+				cur->next = pivot;
+			if (pivot)
+				pivot->prev = cur;
+		}
+	}
 }
 
