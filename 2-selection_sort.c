3b794427ec902fa0a90013fdaeea9c020c6c0952
GetacherD <getacher.d@gmail.com>
2022-07-16 00:37:02 -0700

    optimize for sorted

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,7 +6,7 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	int min, tmp, ok_swap = 0;
+	int min, tmp, ok_swap = 0, stop = 0;
 	size_t i, j = 1, J;
 
 	if (!array || size == 1)
@@ -14,6 +14,8 @@ void selection_sort(int *array, size_t size)
 	min = array[0];
 	for (i = 0; i < size - 1; i++)
 	{
+		if (array[i] > array[i + 1])
+			stop = 0;
 		while (j < size)
 		{
 			if (array[j] <= min)
@@ -22,6 +24,8 @@ void selection_sort(int *array, size_t size)
 				J = j;
 				ok_swap = 1;
 			}
+			if (j < size - 1 && array[j] > array[j + 1])
+				stop = 0;
 			j++;
 		}
 		if (ok_swap)
@@ -34,6 +38,10 @@ void selection_sort(int *array, size_t size)
 		}
 		else
 			min = array[i + 1];
+		if (stop == 1)
+			break;
+		else
+			stop = 1;
 		ok_swap = 0;
 		j = i + 2;
 	}
