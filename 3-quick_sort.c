35c938ecf46ce3395d508618df7e54d46870c5a9
GetacherD <getacher.d@gmail.com>
2022-07-17 13:57:12 -0400

    format print

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,58 +1,54 @@
 #include "sort.h"
 
+
 /**
- * qs - quick sort recursive helper function
- * @array: array to be sorted
- * @size: size of array
- * @left: start index
- * @right: end index
- * @odd: when to print
+ * sort_pivot-sort the list by finding the pivot number
+ * @array:array of list
+ * @start:list start from 0
+ * @end:list ends with size- 1
+ * @size:end of list
+ * Return:index of list
  */
-void qs(int *array, size_t size, int left, int right, int *odd)
+
+void  sort_pivot(int *array, int start, int end, size_t size)
 {
-	int pivot = array[right];
-	int i = left - 1, tmp;
-	int j =  left;
+	int i, j, temp;
 
-	if (left >= right)
+	if (start >= end)
 		return;
-
-	*odd = *odd + 1;
-	for (j = left; j < right; j++)
+	i = start - 1;
+	for (j = start; j < end; j++)
 	{
-		if (array[j] < pivot)
+		if (array[j] < array[end])
 		{
 			i++;
-			tmp = array[j];
-			array[j] = array[i];
-			array[i] = tmp;
+			if (i != j)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
 		}
 	}
 	i++;
-	if (((*odd) % 2) == 0)
+	if (array[i] >  array[end])
+	{
+		temp = array[i];
+		array[i] = array[end];
+		array[end] = temp;
 		print_array(array, size);
-	tmp = array[right];
-	array[right] = array[i];
-	array[i] = tmp;
-	if (((*odd) % 2) == 1)
-		print_array(array, size);
-	qs(array, size, left, i - 1, odd);
-	qs(array, size, i + 1, right, odd);
+	}
+	sort_pivot(array, start, i - 1, size);
+	sort_pivot(array, i + 1, end, size);
 }
 /**
- * quick_sort - sort array using quick sort algorith
- * @array: array to be sorted
- * @size: size of array
+ * quick_sort-write a function that print sort order
+ * @array:list of array
+ * @size:list size
  */
+
 void quick_sort(int *array, size_t size)
 {
-	int k = 0;
-	int *odd = &k;
-
-	if (!array || size == 1)
-	{
-		return;
-	}
-	qs(array, size,  0, size - 1, odd);
-	print_array(array, size);
+	sort_pivot(array, 0, (int)size - 1, size);
 }
