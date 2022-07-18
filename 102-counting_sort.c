852060386190a7ba9f710f681782959b9be5b32e
GetacherD <getacher.d@gmail.com>
2022-07-18 07:20:56 -0400

    fix for repitiive val

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -18,7 +18,30 @@ int count_n(int n, int *array, size_t size)
 	}
 	return (count);
 }
+/**
+ * copy_data -copy sorted array to origional
+ * @array: array to be sorted
+ * @size: size of array
+ */
+void copy_data(int *array, size_t size)
+{
+	size_t i;
+	int c, j;
 
+	for (i = 0; i < size; i++)
+	{
+		c = array[i];
+		if (c > 0)
+		{
+			j = 1;
+			while (array[i - j] == 0 && (((int)i - j) >= 0))
+			{
+				array[i - j] = array[i];
+				j++;
+			}
+		}
+	}
+}
 /**
  * counting_sort - sort array with counting algorithm
  * @array: array to be sorted
@@ -57,9 +80,10 @@ void counting_sort(int *array, size_t size)
 	printf("%d\n", index[max]);
 	array_index = (int *)malloc(sizeof(int) * size);
 	for (i = 0; i < size; i++)
-		array_index[index[value[array[i]] - 1]] = array[i];
+		array_index[index[value[array[i]]] - 1] = array[i];
 	for (i = 0; i < size; i++)
 		array[i] = array_index[i];
+	copy_data(array, size);
 	free(array_index);
 	free(value);
 	free(index);
