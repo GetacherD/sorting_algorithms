91dc44e351342ab2f8db8881b8a3b08e72029728
GetacherD <getacher.d@gmail.com>
2022-07-20 01:53:42 -0400

    fix NULL

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -55,7 +55,7 @@ void counting_sort(int *array, size_t size)
 	int c, j;
 	int *array_index;
 
-	if (size == 1)
+	if (array == NULL ||size == 1)
 		return;
 	for (i = 0; i < size; i++)
 		if (array[i] > max)
